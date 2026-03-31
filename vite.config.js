0885e6526340cbd65bfaca7e9f886c2f22875993
azeem460 <azeemfive12@gmail.com>
2026-03-31 13:11:03 +0500

    fix base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ecommerce-website/',
   plugins: [react()],
+  base: '/Ecommerce-Website/', // Replace with your EXACT repo name
 })
+
